cc1c43cd9c0d3fb45df76716194b93cc92448c66
EC2 Default User <ec2-user@ip-172-31-2-39.ap-northeast-1.compute.internal>
2022-11-09 10:55:37 +0000

    途中

--- dentaku.js
+++ dentaku.js
@@ -1,17 +1,17 @@
-function clickbutton(target) {
+ function clickbutton(target) {
   let result = document.getElementById("result");
   let target_value = target.innerHTML;
-  console.log(result.innerHTML)
+  console.log(result);
   
-  if (target_value == "AC") {
-    result.innerHTML = "0";
-  } else if (target_value == "=") {
-    result.innerHTML = eval(result.innerHTML)
-  } else {
-    if (result.innerHTML == "0") {
-      result.innerHTML = target_value;
-    } else {
-    result.innerHTML += target_value;
-  }
-}
+//   if (target_value == "AC") {
+//     result.innerHTML = "0";
+//   } else if (target_value == "=") {
+//     result.innerHTML = eval(result.innerHTML)
+//   } else {
+//     if (result.innerHTML == "0") {
+//       result.innerHTML = target_value;
+//     } else {
+//     result.innerHTML += target_value;
+//   }
+// }
 }
