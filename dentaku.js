0f125fcb45d64f40221729ef3d18d78cca6e9942
EC2 Default User <ec2-user@ip-172-31-2-39.ap-northeast-1.compute.internal>
2022-11-14 09:58:59 +0000

    ＋のみ連続入力できてしまう

--- dentaku.js
+++ dentaku.js
@@ -2,7 +2,6 @@
  
  function clickbutton(target) {
   let result = document.getElementById("result");
-  console.log(result);
   let target_value = target.innerHTML;
   
   if (target_value == "AC") {
@@ -17,9 +16,11 @@
     result.innerHTML += target_value;
   } if (result.innerHTML.slice(-1) === result.innerHTML.slice(-1) == "+" || result.innerHTML.slice(-1) == "-" || result.innerHTML.slice(-1) == "*" || result.innerHTML.slice(-1) == "/") {
    result.innerHTML = target_value;
+  console.log(result.innerHTML);
+   }
 }
 }
-}
+
 
 // if (result.slice(-1) == "+" || result.slice(-1) == "-" || result.slice(-1) == "*" || result.slice(-1) == "/" ) {
 //   result.innerHTML = result.innerHTML.replace(-2, -1);  
