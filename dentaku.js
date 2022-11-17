41e9431219383621cd88872537a0e5a23c082869
EC2 Default User <ec2-user@ip-172-31-2-39.ap-northeast-1.compute.internal>
2022-11-17 11:03:24 +0000

    インデントの整理後

--- dentaku.js
+++ dentaku.js
@@ -1,27 +1,33 @@
-
  
- function clickbutton(target) {
-  let result = document.getElementById("result");
-  let target_value = target.innerHTML;
-  
-  if (target_value == "AC") {
-   // console.log("0");
-    result.innerHTML = "0";
-  } else if (target_value == "=") {
-    result.innerHTML = eval(result.innerHTML)
-  } else if (result.innerHTML == "0") {
-      result.innerHTML = target_value;
-  } else if (result.innerHTML.slice(-1) == "+" || 
-        result.innerHTML.slice(-1) == "-" || 
-        result.innerHTML.slice(-1) == "*" || 
-        result.innerHTML.slice(-1) == "." ||
-        result.innerHTML.slice(-1) == "/") {
-   result.innerHTML = target_value;
-  console.log(result.innerHTML);
-    } else {
-    result.innerHTML += target_value;
-}
-}
+ 　function clickbutton(target) {
+     let result = document.getElementById("result");
+     let target_value = target.innerHTML;
+     
+     if (target_value == "AC") {
+       result.innerHTML = "0";
+       return;
+     }
+     
+     if (target_value == "=") {
+       result.innerHTML = eval(result.innerHTML)
+       return;
+     } 
+     if (result.innerHTML == "0") {
+         result.innerHTML = target_value;
+         return;
+         
+     } if (result.innerHTML.slice(-1) == "+" || 
+           result.innerHTML.slice(-1) == "-" || 
+           result.innerHTML.slice(-1) == "*" || 
+           result.innerHTML.slice(-1) == "." ||
+           result.innerHTML.slice(-1) == "/") {
+         result.innerHTML = target_value;
+         return;
+       } 
+       
+       result.innerHTML += target_value;
+   }
+
 
 
 // if (result.slice(-1) == "+" || result.slice(-1) == "-" || result.slice(-1) == "*" || result.slice(-1) == "/" ) {
